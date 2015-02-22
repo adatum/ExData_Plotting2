ff82f1ab4e8bdee585821fb043ebce3394d7eab3
adatum <adatum@users.noreply.github.com>
2015-02-22 18:39:14 -0500

    adds plot1.R and plot1.png

---
+++ plot1.R
@@ -0,0 +1,45 @@
+# plot1.R
+# Exploratory Data Analysis: Course Project 2
+# Author: adatum
+#
+# Assumes the data files in working directory:
+#       summarySCC_PM25.rds
+#       Source_Classification_Code.rds
+#
+# Otherwise, downloads and unzips them from URL:
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip
+#
+
+dataURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+fileNEI <- "summarySCC_PM25.rds"
+fileSCC <- "Source_Classification_Code.rds"
+
+# if data files do not exist, download and unzip them
+if(!file.exists(fileNEI) & !file.exists(fileSCC)){
+        download.file(url = dataURL, destfile = "data.zip", method = "curl")
+        unzip("data.zip")
+}
+
+# save time by loading data objects only if they do not already exist
+if(!exists("NEI") & !exists("SCC")){
+        NEI <- readRDS(fileNEI)
+        SCC <- readRDS(fileSCC)
+}
+
+tot_e <- NEI %>% 
+        group_by(year) %>% 
+        filter(year %in% c(1999, 2002, 2005, 2008)) %>% 
+        summarize(total_emissions = sum(Emissions))
+
+png("plot1.png")
+
+barplot(tot_e$total_emissions, 
+        names.arg = tot_e$year,
+        col = "darkblue",
+        main = "Total emissions from PM2.5 in United States",
+        xlab = "Year",
+        ylab = "Total emissions [tons]"
+        )
+
+dev.off()
+
